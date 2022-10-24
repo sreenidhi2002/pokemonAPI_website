8ebd16b24055afbd2627e2897b28f5cb6f4c35d3
sreenidhivijay <sreenidhi5102@gmail.com>
2022-10-23 19:27:46 -0500

    MP2 fix again 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 
 import React from 'react';
 import { Component } from 'react';
-import { HashRouter, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 
 import Search from './Component/SearchView/Search.js';
 import Gallery from './Component/GalleryView/Gallery.js';
@@ -21,21 +21,21 @@ class App extends Component {
   render() {
     return (
       // Use of HashRouter and process.env.PUBLIC_URL as per instructions from TAs in Piazza Posts
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className='navtop'>
         <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg" className="picture_main" alt='Pokedex logo'></img>
         <div className='navtopHeading'>Pokedex Entries</div>
         </div>
           <div className='NavButton'>
-            <Link to="/search">Search</Link>
+            <Link to="/">Search</Link>
             <Link to="/gallery">Gallery</Link>
           </div>
         <Routes>
           <Route path="/gallery" element={<Gallery data = {this.state.data}/>}/>
-          <Route path="/search" element={<Search data = {this.state.data}/>}/>
+          <Route path="/" element={<Search data = {this.state.data}/>}/>
           <Route path="/detail/:id" element={<DetailTest data = {this.state.data}/>}/>
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     );
   }
 }
